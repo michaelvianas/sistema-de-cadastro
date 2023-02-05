64baed4e8b3409c412d53548116df9a10373a010
michaelvianas <vmichael116@gmail.com>
2023-02-05 18:11:50 -0300

    Corrigindo erro de commit

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 console.log(`Rodando o sistema de gerenciamento de jogos`);
 console.log(`Aplicação está se conectado ao banco de dados`);
 // mais um console.log de teste:
-console.log(`Enviando para o Github`);
+console.log(`Enviando para o GitHub`);
 
