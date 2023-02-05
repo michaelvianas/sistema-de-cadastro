7f12d02e00628e91ae0a727a7ce55b067270efd0
michaelvianas <vmichael116@gmail.com>
2023-02-05 18:11:15 -0300

    Adicionnado um console.log ao app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 console.log(`Rodando o sistema de gerenciamento de jogos`);
 console.log(`Aplicação está se conectado ao banco de dados`);
 // mais um console.log de teste:
-console.log(`Michael fez este commit sem você ver na aula`);
+console.log(`Enviando para o Github`);
 
